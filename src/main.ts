11800b77dc1a37c733ac770ce3b5527372b2855c
v84394 <v84394@AH990420>
2026-06-01 12:37:18 -0600

    ejercicios de1 a 3

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 ﻿//import './style.css';
+//modulo encapsulacion de un procedimiento
+//import './topics/01-basic-types';
+//import './topics/02-objet-interface'
+import './topics/03-functions'
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
