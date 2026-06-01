ad0a864b551c9a8a5261c710f9e043cff276ed30
v84394 <v84394@AH990420>
2026-06-01 13:13:39 -0600

    video de destructuracion

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@
 //import './topics/01-basic-types';
 //import './topics/02-objet-interface'
 //import './topics/03-functions'
-import './topics/04-homework'
+//import './topics/04-homework'
+import './topics/05-basic-destructuring'
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
 app.innerHTML = 'Hola Mundo';
---
+++ src/topics/05-basic-destructuring.ts
@@ -0,0 +1,41 @@
+//la destructuracion se puede aplicar en objetos o modulosm argumentsos o valriables
+//tomar ciertas piezas en vez de venir campo por campo
+interface AudioPlayer{
+    audioVolume:number;
+    songDuration:number;
+    songName:string;
+    details:Details;
+}
+interface Details{
+    author:string;
+    year:number
+}
+const audioPlayer:AudioPlayer={
+    audioVolume:0,
+    songDuration:0,
+    songName:"Mess",
+    details:{
+        author:"EDD",
+        year:2015,
+    }
+}
+//console.log(audioPlayer.songName)
+//console.log(audioPlayer.details.author)
+
+//desestructuracios
+const songName ='Happy Song'
+const {songName:anotherSong} =audioPlayer//le podemos poner un nombre diferente
+
+//destructura de details
+const{details:detalle}=audioPlayer
+const{author:autor}=detalle
+//adentro de uno
+const {
+    songName:name, 
+    songDuration:duration,
+    details:{author}
+} =audioPlayer
+
+
+console.log(author)
+export{};
